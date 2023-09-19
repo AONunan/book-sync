e4a488c64cd56136f35a06f3422d3d9a097ec7bd
Alan Noonan <aganoonan@gmail.com>
2023-09-19 20:31:32 +0100

    Move previously synced books to top of the page

--- app/app.py
+++ app/app.py
@@ -8,16 +8,22 @@ app = Flask(__name__)
 def hello_world():
 
    previously_synced_books = get_book_list()
-   library = {}
+   library = []
 
    for author in os.listdir("/calibre"):
       author_path = f"/calibre/{author}"
 
       if((os.path.isdir(author_path)) and not(author.startswith("."))):
-         library[author] = []
 
          for book in os.listdir(author_path):
-            library[author].append(book)
+
+            library.append({
+               "author": author,
+               "title": book,
+               "previously_synced": book in previously_synced_books
+            })
+   
+   library = sorted(library, key=lambda x: x["previously_synced"], reverse=True)
 
    return render_template('index.html', library=library, previously_synced_books=previously_synced_books)
 
@@ -25,10 +31,14 @@ def hello_world():
 def set_book_list():
    print("'set' API triggered")
 
+   request_data = sorted(json.loads(request.data.decode('utf-8')))
    request_data = json.loads(request.data.decode('utf-8'))
 
+   books_to_sync = request_data["checkedCheckboxes"]
+   books_to_sync.sort()
+
    with open("/db/listofbooks.txt", "w") as file:
-      file.write("\n".join(request_data["checkedCheckboxes"]))
+      file.write("\n".join(books_to_sync))
 
 
    return "Book list updated"
