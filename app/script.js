dffb5e49bed6afb811fd28a66d276ae629646945
Alan Noonan <aganoonan@gmail.com>
2023-09-20 19:34:00 +0100

    Add error message modal

--- app/script.js
+++ app/script.js
@@ -18,6 +18,35 @@ function sendApiRequest(checkedCheckboxes) {
         })
         .catch(error => {
             console.error('Error:', error);
+
+            const modal = document.getElementById('errorModal');
+            const errorMessageElement = document.getElementById('errorMessage');
+
+            // Function to open the modal with an error message
+            function openErrorModal(errorMessage) {
+                errorMessageElement.textContent = errorMessage;
+                modal.style.display = 'block';
+            }
+
+            // Function to close the modal
+            function closeErrorModal() {
+                modal.style.display = 'none';
+            }
+
+            // Event listener to open the modal when the button is clicked
+            openErrorModal(error);
+
+            // Event listener to close the modal when the close button is clicked
+            modal.querySelector('.close').addEventListener('click', closeErrorModal);
+
+            // Event listener to close the modal when the user clicks outside the modal content
+            window.addEventListener('click', (event) => {
+                if (event.target === modal) {
+                    closeErrorModal();
+                }
+            });
+
+
         });
 }
 
